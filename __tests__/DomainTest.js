4427469a9a8cc7616b3020e4cdfda897f8f0e0b1
jayming66 <mjw8941@gmail.com>
2023-11-15 05:31:24 +0900

    test : Discount, Badge 로직 테스트

--- __tests__/DomainTest.js
+++ __tests__/DomainTest.js
@@ -1,5 +1,7 @@
-import Discount from '../src/Model/Discount';
-import Order from '../src/Model/Order';
+import Discount from '../src/Model/Discount.js';
+import Order from '../src/Model/Order.js';
+import { expectedCalculatePayment } from '../src/utils/expectedCalculatePayment.js';
+import Badge from '../src/Model/Badge.js';
 
 describe("도메인 테스트", () => { 
   describe("Order 로직 테스트", () => { 
@@ -33,22 +35,155 @@ describe("도메인 테스트", () => {
   describe("Discount 로직 테스트", () => { 
     //given
     const discountCases = [
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: {
+          '크리스마스 디데이 할인': 1200,
+          '평일 할인': 4046,
+          '주말 할인': 0,
+          '특별 할인': 1000,
+          '증정 이벤트': 25000,
+        }, date:"3"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: {
+          '크리스마스 디데이 할인': 1100,
+          '평일 할인': 0,
+          '주말 할인': 4046,
+          '특별 할인': 0,
+          '증정 이벤트': 25000,
+        }, date:"2"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: {
+          '크리스마스 디데이 할인': 0,
+          '평일 할인': 4046,
+          '주말 할인': 0,
+          '특별 할인': 0,
+          '증정 이벤트': 25000,
+        }, date:"26"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: {
+          '크리스마스 디데이 할인': 3400,
+          '평일 할인': 4046,
+          '주말 할인': 0,
+          '특별 할인': 1000,
+          '증정 이벤트': 25000,
+        }, date:"25"
+      },
+      {input: "양송이수프-2", expected: {
+          '크리스마스 디데이 할인': 1200,
+          '평일 할인': 0,
+          '주말 할인': 0,
+          '특별 할인': 1000,
+          '증정 이벤트': 0,
+        }, date: "3"
+      },
+      {input: "초코케이크-1", expected: {
+          '크리스마스 디데이 할인': 1200,
+          '평일 할인': 2023,
+          '주말 할인': 0,
+          '특별 할인': 1000,
+          '증정 이벤트': 0,
+        }, date: "3"
+      },
       {input: "양송이수프-1", expected: {
           '크리스마스 디데이 할인': 0,
           '평일 할인': 0,
           '주말 할인': 0,
           '특별 할인': 0,
           '증정 이벤트': 0,
-        }},
+        }, date: "3"
+      },
     ];
-    test.each(discountCases)('혜택 내역 테스트', async ({ input,expected }) => {
+    test.each(discountCases)('혜택 내역 테스트', async ({ input,expected,date }) => {
       //when
       const order = new Order(input);
-      const date = '3';
       const discount = new Discount(order, date);
+      await discount.calculateDiscount();
       const totalDiscount = discount.getDiscount();
       //then
       expect(totalDiscount).toEqual(expected);
+    })
+
+    //given
+    const totalBenefitCases = [
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: 31246, date:"3"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: 30146, date:"2"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: 29046, date:"26"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: 33446, date:"25"
+      },
+      {input: "양송이수프-2", expected: 2200, date: "3"
+      },
+      {input: "초코케이크-1", expected: 4223, date: "3"
+      },
+      {input: "양송이수프-1", expected: 0, date: "3"
+      },
+    ];
+    test.each(totalBenefitCases)('총혜택 금액 테스트', async ({ input,expected,date }) => {
+      //when
+      const order = new Order(input);
+      const discount = new Discount(order, date);
+      await discount.calculateDiscount();
+      const totalBenefit = discount.getTotalDiscount();
+      //then
+      expect(totalBenefit).toEqual(expected);
+    })
+
+    //given
+    const totalPaymentCases = [
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: 135754, date:"3"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: 136854, date:"2"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: 137954, date:"26"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: 133554, date:"25"
+      },
+      {input: "양송이수프-2", expected: 9800, date: "3"
+      },
+      {input: "초코케이크-1", expected: 10777, date: "3"
+      },
+      {input: "양송이수프-1", expected: 6000, date: "3"
+      },
+    ];
+    test.each(totalPaymentCases)('할인 후 예상 결제 금액 테스트', async ({ input,expected,date }) => {
+      //when
+      const order = new Order(input);
+      const discount = new Discount(order, date);
+      await discount.calculateDiscount();
+      const isPresent = order.getIsPresent();
+      const totalPayment = expectedCalculatePayment(order.getTotalPrice(), discount.getTotalDiscount(), isPresent);
+      //then
+      expect(totalPayment).toEqual(expected);
+    })
+  })
+  describe("Badge 로직 테스트", () => { 
+    //given
+    const badgeCases = [
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: '산타', date:"3"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: '산타', date:"2"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: '산타', date:"26"
+      },
+      {input: "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", expected: '산타', date:"25"
+      },
+      {input: "양송이수프-2", expected: '없음', date: "3"
+      },
+      {input: "초코케이크-1", expected: '없음', date: "3"
+      },
+      {input: "양송이수프-1", expected: '없음', date: "3"
+      },
+    ];
+    test.each(badgeCases)('12월 이벤트 배지 테스트', async ({ input,expected,date }) => {
+      //when
+      const order = new Order(input);
+      const discount = new Discount(order, date);
+      await discount.calculateDiscount();
+      const totalDiscount = discount.getTotalDiscount();
+      const badge = new Badge(totalDiscount);
+      //then
+      expect(badge.getBadge()).toEqual(expected);
     })
   })
 
