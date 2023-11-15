e612baec1d06d2d00fcdeed6c3f278421fb70893
jayming66 <mjw8941@gmail.com>
2023-11-15 19:15:27 +0900

    refactor : Discount 클래스 - 필드 수 줄이기

--- src/Model/Discount.js
+++ src/Model/Discount.js
@@ -7,15 +7,16 @@ import { menuDiscountCalculator } from '../utils/menuDiscountCalculator.js';
 class Discount {
   #order;
   #date;
-  #weekendDiscount = MONEY.zero;
-  #weekdayDiscount = MONEY.zero;
-  #specialDiscount = MONEY.zero;
-  #christmasDiscount = MONEY.zero;
-  #totalDiscount = MONEY.zero;
+  #discounts = new Map();
 
   constructor(order, date) {
     this.#order = order;
     this.#date = date;
+    this.#discounts.set('weekendDiscount', 0);
+    this.#discounts.set('weekdayDiscount', 0);
+    this.#discounts.set('specialDiscount', 0);
+    this.#discounts.set('christmasDiscount', 0);
+    this.#discounts.set('totalDiscount', 0);
   }
 
   async calculateDiscount() {
@@ -33,7 +34,8 @@ class Discount {
   async #calculateWeekendDiscount() {
     if ((this.#date >= DATE.firstDay && this.#date <= DATE.TWO) || (this.#date >= DATE.EIGHT && this.#date <= DATE.NINE) || (this.#date >= DATE.FIFTEEN && this.#date <= DATE.SIXTEEN) || (this.#date >= DATE.TWENTY_TWO && this.#date <= DATE.TWENTY_THREE) || (this.#date >= DATE.TWENTY_NINE && this.#date <= DATE.THIRTY)) {
       const discount = menuDiscountCalculator(CATEGORIES.main, this.#order);
-      this.#weekendDiscount += discount;
+      const currentweekendDiscount = this.#discounts.get('weekendDiscount');
+      this.#discounts.set('weekendDiscount', currentweekendDiscount + discount);
     }
       
   }
@@ -41,43 +43,47 @@ class Discount {
   async #calculateWeekdayDiscount() { 
     if ((this.#date >= DATE.THREE && this.#date <= DATE.SEVEN) || (this.#date >= DATE.TEN && this.#date <= DATE.FOURTEEN) || (this.#date >= DATE.SEVENTEEN && this.#date <= DATE.TWENTY_ONE) || (this.#date >= DATE.TWENTY_FOUR && this.#date <= DATE.TWENTY_EIGHT) || (this.#date >= DATE.lastDay)) {
       const discount = menuDiscountCalculator(CATEGORIES.dessert, this.#order);
-      this.#weekdayDiscount += discount;
+      const currentweekdayDiscount = this.#discounts.get('weekdayDiscount');
+      this.#discounts.set('weekdayDiscount', currentweekdayDiscount + discount);
     }
   }
 
   async #calculateSpecialDiscount() {
-    if ((this.#date == DATE.THREE || this.#date == DATE.TEN || this.#date == DATE.SEVENTEEN || this.#date == DATE.TWENTY_FOUR || this.#date == DATE.TWENTY_FIVE || this.#date == DATE.lastDay)){
-      this.#specialDiscount = MONEY.thousand;
+    if ((this.#date == DATE.THREE || this.#date == DATE.TEN || this.#date == DATE.SEVENTEEN || this.#date == DATE.TWENTY_FOUR || this.#date == DATE.TWENTY_FIVE || this.#date == DATE.lastDay)) {
+      this.#discounts.set('specialDiscount', MONEY.thousand);
     }
   }
 
   async #calculateChristmasDiscount() {
     if (this.#date <= DATE.TWENTY_FIVE && this.#date >= DATE.firstDay) {
       const discount = (this.#date - ONE) * MONEY.christmasDiscountUnit;
-      this.#christmasDiscount = discount + MONEY.thousand;
+      this.#discounts.set('christmasDiscount', discount + MONEY.thousand);
+      
     }
   }
   
   async #calculateTotalDiscount() { 
-    this.#totalDiscount = this.#weekendDiscount + this.#weekdayDiscount + this.#specialDiscount + this.#christmasDiscount;
+    const totalDiscount = this.#discounts.get('weekendDiscount') + this.#discounts.get('weekdayDiscount') + this.#discounts.get('specialDiscount') + this.#discounts.get('christmasDiscount');
+    this.#discounts.set('totalDiscount', totalDiscount);
     if (this.#order.getIsPresent()) {
-      this.#totalDiscount += MONEY.presentPrice;
+      const presentTotalDiscount = this.#discounts.get('totalDiscount');
+      this.#discounts.set('totalDiscount', presentTotalDiscount + MONEY.presentPrice);
     }
   }
 
   getDiscount() {
     const discount = {
-      '크리스마스 디데이 할인': this.#christmasDiscount,
-      '평일 할인': this.#weekdayDiscount,
-      '주말 할인': this.#weekendDiscount,
-      '특별 할인': this.#specialDiscount,
+      '크리스마스 디데이 할인': this.#discounts.get('christmasDiscount'),
+      '평일 할인': this.#discounts.get('weekdayDiscount'),
+      '주말 할인': this.#discounts.get('weekendDiscount'),
+      '특별 할인': this.#discounts.get('specialDiscount'),
       '증정 이벤트': this.#order.getIsPresent() ? MONEY.presentPrice : MONEY.zero,
     }
     return discount;
   }
 
   getTotalDiscount() {
-    return this.#totalDiscount;
+    return this.#discounts.get('totalDiscount')
   }
 }
 
